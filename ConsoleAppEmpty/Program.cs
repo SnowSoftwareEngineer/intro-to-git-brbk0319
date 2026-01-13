b6d59b16d1c7d259392ae02251d0bedfd5552e3f
bryelle.kimball <bryelle.kimball@students.snow.edu>
2026-01-13 15:05:16 -0700

    Spacing and prettiness

--- ConsoleAppEmpty/Program.cs
+++ ConsoleAppEmpty/Program.cs
@@ -4,17 +4,17 @@ public class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Select a Difficulty:\nEasy\nMedium\nHard");
+        Console.WriteLine("Select a Difficulty:\n     Easy\n     Medium\n     Hard");
 
         string difficultyInput = Console.ReadLine();
 
         if (Enum.TryParse(difficultyInput, true, out GameEnums.GameDifficulty difficultyLevel))
         {
-            Console.WriteLine("Select a Class:\nWarrior\nMage\nRogue");
+            Console.WriteLine("Select a Class:\n     Warrior\n     Mage\n     Rogue");
             string classInput = Console.ReadLine();
             if (Enum.TryParse(classInput, true, out GameEnums.CharacterClass characterClass))
             {
-                Console.WriteLine($"You selected:\nDifficulty: {difficultyLevel}\nCharacter Class: {characterClass}");
+                Console.WriteLine($"You selected:\n     Difficulty: {difficultyLevel}\n     Character Class: {characterClass}");
                 
             }
             else { Console.WriteLine("Ope, watch the cases there buddy."); }
