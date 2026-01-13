f904e84d4688bd5b558a0f058a87e61bbe1db88d
bryelle.kimball <bryelle.kimball@students.snow.edu>
2026-01-13 14:58:28 -0700

    awesome game menu commit

--- ConsoleAppEmpty/Program.cs
+++ ConsoleAppEmpty/Program.cs
@@ -1,10 +1,28 @@
-﻿public class Program
+﻿using GameEnums;
+
+public class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World!");
-        //hello again
-        //one more test
-        Console.WriteLine("Moin aus Ephraim Utah");
+        Console.WriteLine("Select a Difficulty:\nEasy\nMedium\nHard");
+
+        string difficultyInput = Console.ReadLine();
+
+        if (Enum.TryParse(difficultyInput, true, out GameEnums.GameDifficulty difficultyLevel))
+        {
+            Console.WriteLine("Select a Class:\nWarrior\nMage\nRogue");
+            string classInput = Console.ReadLine();
+            if (Enum.TryParse(classInput, true, out GameEnums.CharacterClass characterClass))
+            {
+                Console.WriteLine($"You selected:\nDifficulty: {difficultyLevel}\nCharacter Class: {characterClass}");
+                
+            }
+            else { Console.WriteLine("Ope, watch the cases there buddy."); }
+        }
+        else
+        {
+            Console.WriteLine("haha real funny. Try again.");
+        }
+
     }
 }
